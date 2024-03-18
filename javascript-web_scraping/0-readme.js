04aa50e17c2eadcfdaadf98c8119d268589343c3
samu-rize <6710@holbertonstudents.com>
2024-03-18 23:41:11 +0100

    fix code

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 // a script that reads and prints the content of a file.
-import { readFile } from 'fs'
-const fileName = process.argv[2]
-readFile(fileName, 'utf8', (err, data) => {
+const fs = require('fs');
+const fileName = process.argv[2];
+fs.readFile(fileName, 'utf8', (err, data) => {
   if (err) {
-    console.log(`Error reading file "${fileName}":`, err)
+    console.log(`Error reading file "${fileName}":`, err);
   } else {
-    console.log(data)
+    console.log(data);
   }
-})
+});
