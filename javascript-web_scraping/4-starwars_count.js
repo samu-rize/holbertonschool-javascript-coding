09d1f24a9901a1dc913ecdd5d796d33cec679443
samu-rize <6710@holbertonstudents.com>
2024-03-27 05:15:51 +0100

    Refactor star wars character count logic

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,23 +1,17 @@
 #!/usr/bin/node
 
-const request = require('request');
+const request = require("request");
 const url = process.argv[2];
+const id = '18';
 
-let count = 0;
-
-request(url, function (err, response, body) {
+request({method: "GET", url: url}, function(err, response, body) {
   if (err) {
-    console.log(err);
+    console.error(err);
   } else {
-    const results = JSON.parse(body).results;
-    for (const result of results) {
-      const characters = result.characters;
-      for (const character of characters) {
-        if (character.includes('18')) {
-          count++;
-        }
-      }
-    }
-    console.log(count);
+    const result = JSON.parse(body).results;
+    const moviesFiltered = result.filter((film) =>
+      film.characters.some((character) => character.includes(id))
+    );
+    console.log(moviesFiltered.length);
   }
 });
