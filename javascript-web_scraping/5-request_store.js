2bf4cad56ab4526ef79652c4eb536f890210411a
samu-rize <6710@holbertonstudents.com>
2024-03-27 06:33:28 +0100

    Refactor request and fs imports in 5-request_store.js

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,17 +1,16 @@
 #!/usr/bin/node
 
-const request = require("request");
+const request = require('request');
 const url = process.argv[2];
 const fileName = process.argv[3];
-const fs = require("fs");
+const fs = require('fs');
 
-request({method: 'GET', url: url}, (err, response, body) => {
+request({ method: 'GET', url }, (err, response, body) => {
   if (err) {
     console.error(err);
   } else {
-    fs.writeFile(fileName, body, "utf-8", (err) => {
-      if (err)
-        console.error(err);
+    fs.writeFile(fileName, body, 'utf-8', (err) => {
+      if (err) { console.error(err); }
     });
   }
 });
